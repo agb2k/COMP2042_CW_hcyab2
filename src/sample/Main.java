952f9a1046350387a3fcecfd997172e410b47c46
Abhinav <abhinav.basil@gmail.com>
2020-11-03 19:08:11 +0800

    Changed the title to "Hey"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hey Github");
+        primaryStage.setTitle("Hey");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
